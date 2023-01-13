fb115c4ea88188c1ce2f6a2ba4a2952ad488b9df
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-13 22:08:46 +0800

    update,

--- helloworld-v2/myAppName/src/blitz-client.js
+++
@@ -1,9 +0,0 @@
-import { AuthClientPlugin } from "@blitzjs/auth"
-import { setupBlitzClient } from "@blitzjs/next"
-import { BlitzRpcPlugin } from "@blitzjs/rpc"
-export const authConfig = {
-  cookiePrefix: "myAppName",
-}
-export const { withBlitz } = setupBlitzClient({
-  plugins: [AuthClientPlugin(authConfig), BlitzRpcPlugin({})],
-})
